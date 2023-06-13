24ad4dc0754927d2b920ac0f2d888911a6f6b78a
zhygerAITU <zhyger.0001@gmail.com>
2023-06-13 18:41:28 +0600

    GradientDescentMulti
    Solved GradientDescents using vectorization!

--- Assigment-Week-1/gradientDescentMulti.m
+++ Assigment-Week-1/gradientDescentMulti.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -17,15 +18,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    h = X * theta;
 
 
 
-
-
-
-
-
-
+    theta = theta - ((transpose(X) * (h - y)) / m) * alpha;
 
     % ============================================================
 
